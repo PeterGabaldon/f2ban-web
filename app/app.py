1727fb7e99bbac3bf193823c5333750dfbe36435
Pedro Gabaldon <34518201+PeterGabaldon@users.noreply.github.com>
2022-09-07 22:10:35 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ DB = "/var/lib/fail2ban/fail2ban.sqlite3"
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = g._database = sqlite3.connect(DB)
     return db
 
 @app.teardown_appcontext
